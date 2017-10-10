2399f56e1842d809af145393457a26678467acc6
chandrasekarvelraj <vchandras@mindfiresolutions.com>
2017-10-11 01:47:29 +0530

    whats app message changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
 
   def display
-  	a = "Click%20on%20this%20below%20link%20to%20view%20_*DIWALI%20WISHES*_%20from%20your%20friend%0A%0A"
+  	a = "Click%20on%20the%20link%20to%20view%20_*DIWALI%20WISHES*_%20from%20your%20friend%0A%0A"
   	b = request.protocol+request.host_with_port+request.fullpath
   	c =  "whatsapp://send?text=_*'''Create%20your%20own%20greeting...!!!'''*_%0A%0A"
   	@d = c + a + b
